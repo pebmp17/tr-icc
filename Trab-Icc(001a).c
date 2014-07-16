57b4c89b0593537fd873db28b1c3ed2436fe1a5c
pebmp17 <pedrobmp3@gmail.com>
2014-07-16 11:03:44 -0300

    #04

--- Trab-Icc(001a).c
+++ Trab-Icc(001a).c
@@ -7,73 +7,92 @@ int mGerenCard();
 int mkPedido();
 int soliConta();
 
- int main(void)
+int main(void)
 {
-	int optmainmenu= 1;
-	while (optmainmenu != 4){
-		puts ("Bem Vindo.");
-		puts ("Quais dessas opçoes voce deseja usar?");
-		puts ("1. Gerenciar itens do cardapio");
-		puts ("2. Fazer um Pedido");
-		puts ("3. Solicitar Conta");
-		puts ("4. Sair do sistema");
-		scanf ("%d", &optmainmenu);
-		switch (optmainmenu){
-			case 1:
-				puts("Opção escolhida: 1.Gerenciar itens do cardapio");
-				mGerenCard();
-				break;
-			case 2:
-				puts("Opção escolhida: 2.Fazer um Pedido");
-				mkPedido();
-				break;
-			case 3:
-				puts ("Opção escolhida: 3.Solicitar Conta");
-				soliConta();
-				break;
-			case 4:
-				puts ("Opção escolhida: 4.Sair do Sistema");
-				continue;
-			default :
-				puts("\nOpção Invalida");
-		}
-	}
-	return 0;
+    char optmainmenu='1';
+    puts ("Bem Vindo.");
+    puts ("Quais dessas opcoes voce deseja usar?");
+    puts ("1. Gerenciar itens do cardapio");
+    puts ("2. Fazer um Pedido");
+    puts ("3. Solicitar Conta");
+    puts ("4. Sair do sistema");
+    gets (&optmainmenu);
+    while ((optmainmenu>'4') ||(optmainmenu<'1'))
+    {
+        puts("Opcao Invalida");
+        puts ("Quais dessas opcoes voce deseja usar?");
+        puts ("1. Gerenciar itens do cardapio");
+        puts ("2. Fazer um Pedido");
+        puts ("3. Solicitar Conta");
+        puts ("4. Sair do sistema");
+        gets(&optmainmenu);
+    }
+    switch (optmainmenu)
+    {
+    case '1':
+        puts("Opcao escolhida: 1.Gerenciar itens do cardapio");
+        mGerenCard();
+        break;
+    case '2':
+        puts("Opcao escolhida: 2.Fazer um Pedido");
+        mkPedido();
+        break;
+    case '3':
+        puts ("Opcao escolhida: 3.Solicitar Conta");
+        soliConta();
+        break;
+    case '4':
+        puts ("Opcao escolhida: 4.Sair do Sistema");
+        break;
+    }
+    return 0;
 }
 
-int mGerenCard(){
-	int strcmp(const char*__s1, const char*__s2);
-	int menuGerenBebidas();
-	int menuGerenComidas();
-	char bOUc[15];
-	int scape = 0;
-	scanf("%s", bOUc);
-	while (scape != 1){
-		puts ("Voce deseja gerenciar bebidas ou comidas?");
-		if((strcmp(bOUc, "Bebidas") == 0) ||  (strcmp(bOUc, "Bebidas")) == 0){
-			scape = 1;
-		  menuGerenComidas();
-		}
-		else if ((strcmp(bOUc,"Comidas") == 0) || (strcmp(bOUc,"comidas") == 0)){
-			scape = 1;
-			menuGerenBebidas();
-		}
-		else 
-		{
-			puts("Só é possivel gerenciar Comidas ou Bebidas");
-		}
-	}
-	return 0;
-	
+int mGerenCard()
+{
+    /*int strcmp(const char*__s1, const char*__s2); */
+    int menuGerenBebidas();
+    int menuGerenComidas();
+    char bOUc[15];
+    int scape = 0;
+    while (scape != 1)
+    {
+        puts ("Voce deseja gerenciar bebidas ou comidas?");
+        scanf("%s", bOUc);
+        if((strcmp(bOUc, "Bebidas") == 0) || (strcmp(bOUc, "bebidas")) == 0)
+        {
+            scape = 1;
+            puts("Bebidas\n");
+            menuGerenComidas();
+        }
+        else if ((strcmp(bOUc,"Comidas") == 0) || (strcmp(bOUc,"comidas") == 0))
+        {
+            scape = 1;
+            puts("Comidas\n");
+            menuGerenBebidas();
+        }
+        else
+        {
+            puts("So e possivel gerenciar Comidas ou Bebidas");
+        }
+    }
+    return 0;
+
 }
 int menuGerenBebidas()
-	{
-		return 0;
-	}
-
-int mkPedido(){
-	return 0;
+{
+    return 0;
 }
-int soliConta(){
-	return 0;
+int menuGerenComidas()
+{
+    return 0;
+}
+
+int mkPedido()
+{
+    return 0;
+}
+int soliConta()
+{
+    return 0;
 }
